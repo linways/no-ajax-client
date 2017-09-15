606362f21260c7faa9522d0072ec9e936aa2af1e
Rohith Bhaskaran <rohithb@Rohiths-MacBook-Air.local>
2017-09-15 20:08:26 +0530

    moved server part into separate project

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,28 +8,18 @@ module.exports = function(grunt) {
       },
       my_target: {
         files: {
-          'dest/noajax.min.js': ['src/js/noajax.js']
+          'dist/noajax.min.js': ['src/noajax.js']
         }
       }
     },
-    copy: {
-      main: {
-        expand: true,
-        src: 'src/php/noajax.php',
-        dest: 'PHPTestMethods/',
-        flatten: true,
-      },
-    },
     watch: {
-      files: ['src/**/*.js', 'src/**/*.php'],
-      tasks: ['uglify', 'copy']
+      files: ['src/**/*.js'],
+      tasks: ['uglify']
     }
   });
-
+  
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', ['uglify','copy']);
-
+  grunt.registerTask('default', ['uglify']);
 };
